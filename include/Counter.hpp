b84cdf14f30c4ef9a5f377e9a62a8609ca34d376
SlimRunner <dv.polygon@gmail.com>
2024-05-30 13:50:45 -0700

    Get list of matching cases

--- include/Counter.hpp
+++ include/Counter.hpp
@@ -3,12 +3,14 @@
 
 #include "Choices.hpp"
 
+#include <iterator>
 #include <memory>
 #include <vector>
 
 using Transformer = int (*)(std::vector<int> vars);
 using Predicate = bool (*)(int result);
 using ChoiceVector = std::vector<Choices>;
+using IntCases = std::vector<std::vector<int>>;
 
 class Counter {
 private:
@@ -20,12 +22,16 @@ private:
 
   int enumerateChoice(ChoiceVector::iterator caseIt,
                       std::vector<int>::iterator varIt);
+  int enumerateChoice(ChoiceVector::iterator caseIt,
+                      std::vector<int>::iterator varIt,
+                      std::back_insert_iterator<IntCases> out);
 
 public:
   Counter(Transformer transform, Predicate match);
   Counter();
 
   int countCases();
+  int countCases(std::back_insert_iterator<IntCases> out);
 
   void changeTransformer(Transformer transform);
   void changePredicate(Predicate match);
--- src/Counter.cpp
+++ src/Counter.cpp
@@ -28,11 +28,42 @@ int Counter::enumerateChoice(ChoiceVector::iterator caseIt,
   return res;
 }
 
+int Counter::enumerateChoice(ChoiceVector::iterator caseIt,
+                             std::vector<int>::iterator varIt,
+                             std::back_insert_iterator<IntCases> out) {
+  const int f = caseIt->getFactor();
+  int res = 0;
+  // iterate over ranges of a sinle case
+  for (auto range = caseIt->cbegin(); range < caseIt->cend(); ++range) {
+    // iterate over a single range
+    for (int i = range->first; i <= range->second; ++i) {
+      *varIt = i * f;
+      // if this case is the back of vector call transformer
+      if (caseIt + 1 == mVars.end()) {
+        if (mPred(mTrans(mX))) {
+          ++res;
+          *out++ = mX;
+        }
+      } else {
+        res += enumerateChoice(caseIt + 1, varIt + 1, out);
+      }
+    }
+  }
+  return res;
+}
+
 int Counter::countCases() {
   if (!mTrans && !mPred) {
     return -1;
   }
   return enumerateChoice(mVars.begin(), mX.begin());
+}
+
+int Counter::countCases(std::back_insert_iterator<IntCases> out) {
+  if (!mTrans && !mPred) {
+    return -1;
+  }
+  return enumerateChoice(mVars.begin(), mX.begin(), out);
 }
 
 void Counter::changeTransformer(Transformer transform) { mTrans = transform; }
